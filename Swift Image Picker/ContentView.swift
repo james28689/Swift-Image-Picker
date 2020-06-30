844ea6796c324e2857704924c320a7b859f5c906
James Watling <james.watling@gmail.com>
2020-06-30 16:05:35 +0100

    Initial Commit

---
+++ Swift Image Picker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Image Picker
+//
+//  Created by James Watling on 30/06/2020.
+//  Copyright © 2020 James Watling. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
